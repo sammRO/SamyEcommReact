66d82b94c9d66cb890963bb34d755e5132919381
Tudor Samy <tudorsamy2019@gmail.com>
2023-09-26 17:58:14 +0300

    .

---
+++ src/products.jsx
@@ -0,0 +1,59 @@
+import product1 from './assets/1.png';
+import product2 from './assets/2.png';
+import product3 from './assets/3.png';
+import product4 from './assets/4.png';
+import product5 from './assets/5.png';
+import product6 from './assets/6.webp';
+import product7 from './assets/7.webp';
+import product8 from './assets/8.webp';
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "iPhone",
+        price: 999.0,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: "Macbook Pro 2022 (M1)",
+        price: 1499.0,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: "Nikon 2020 Camera",
+        price: 599.0,
+        productImage: product3,
+    },
+    {
+        id: 4,
+        productName: "Van Go Jacket",
+        price: 199.0,
+        productImage: product4,
+    },
+    {
+        id: 5,
+        productName: "LED Premium Qulity Lights",
+        price: 99.0,
+        productImage: product5,
+    },
+    {
+        id: 6,
+        productName: "Don't Trip Sweater",
+        price: 199.0,
+        productImage: product6,
+    },
+    {
+        id: 7,
+        productName: "Golf T-Shirt",
+        price: 49.0,
+        productImage: product7,
+    },
+    {
+        id: 8,
+        productName: "Don't Trip Cap",
+        price: 79.0,
+        productImage: product8,
+    },
+];
